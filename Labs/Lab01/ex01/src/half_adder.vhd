496ede3eef722dc77aef8b7acbd6d491dc60b94d
Razin936 <md-jubair-salehin.razin@stud.hshl.de>
2026-05-07 10:36:13 +0200

    Update and rename task_01.vhd to half_adder.vhd

---
+++ Labs/Lab01/ex01/src/half_adder.vhd
@@ -0,0 +1,10 @@
+entity half_adder is
+    port(a, b : in bit;
+         s, co : out bit);
+end half_adder;
+
+architecture behavioral of half_adder is
+begin
+    s  <= a xor b;
+    co <= a and b;
+end behavioral;
