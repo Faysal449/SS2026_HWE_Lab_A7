210c82b7e101b9c7f5a944df0815b72a38eeb1bd
Faysal449 <184016333+Faysal449@users.noreply.github.com>
2026-05-17 13:47:28 +0200

    Update BCD_adder_tb.vhd

--- Labs/Lab02/ex04/tb/BCD_adder_tb.vhd
+++ Labs/Lab02/ex04/tb/BCD_adder_tb.vhd
@@ -1,1 +1,55 @@
+-- Testbench for 4-bit BCD Adder
+-- Lab 02 Exercise 04
+
+entity BCD_adder_tb is
+end BCD_adder_tb;
+
+architecture bench of BCD_adder_tb is
+
+    component BCD_adder
+        port(A, B : in  bit_vector(3 downto 0);
+             Ci   : in  bit;
+             S    : out bit_vector(3 downto 0);
+             Co   : out bit);
+    end component;
+
+    signal A_tb, B_tb, S_tb : bit_vector(3 downto 0);
+    signal Ci_tb, Co_tb     : bit;
+
+begin
+
+    uut: BCD_adder port map(A  => A_tb,
+                            B  => B_tb,
+                            Ci => Ci_tb,
+                            S  => S_tb,
+                            Co => Co_tb);
+
+    A_tb <= "0000",
+            "0010" after 10 ns,
+            "0100" after 20 ns,
+            "0101" after 30 ns,
+            "1000" after 40 ns,
+            "1001" after 50 ns,
+            "1001" after 60 ns,
+            "1001" after 70 ns;
+
+    B_tb <= "0000",
+            "0011" after 10 ns,
+            "0101" after 20 ns,
+            "0101" after 30 ns,
+            "0111" after 40 ns,
+            "0001" after 50 ns,
+            "1001" after 60 ns,
+            "1001" after 70 ns;
+
+    Ci_tb <= '0',
+             '0' after 10 ns,
+             '0' after 20 ns,
+             '0' after 30 ns,
+             '0' after 40 ns,
+             '0' after 50 ns,
+             '0' after 60 ns,
+             '1' after 70 ns;
+
+end bench;
 
