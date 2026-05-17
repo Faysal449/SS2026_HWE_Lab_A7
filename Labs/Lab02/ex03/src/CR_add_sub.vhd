4379fe1ec585edeeb64a2c60d14c9c525282931c
Md Azadul Islam <168671197+azadahmed2955@users.noreply.github.com>
2026-05-17 13:58:18 +0200

    Add behavioral model for 1-bit half adder

--- Labs/Lab02/ex03/src/CR_add_sub.vhd
+++ Labs/Lab02/ex03/src/CR_add_sub.vhd
@@ -1,1 +1,14 @@
 
+-- 1-bit Half Adder (behavioral)
+-- Lab 01 Exercise 02
+
+entity half_adder is
+    port(a, b   : in  bit;
+         s, co  : out bit);
+end half_adder;
+
+architecture behavioral of half_adder is
+begin
+    s  <= a xor b;
+    co <= a and b;
+end behavioral;
