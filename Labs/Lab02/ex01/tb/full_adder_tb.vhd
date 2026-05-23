7c41c2b6d1bdf55ae3ff24698999b33add6a2133
Razin936 <md-jubair-salehin.razin@stud.hshl.de>
2026-05-23 13:48:24 +0200

    Upadte Razin

--- Labs/Lab02/ex01/tb/full_adder_tb.vhd
+++ Labs/Lab02/ex01/tb/full_adder_tb.vhd
@@ -1,37 +1,76 @@
-
-
--- Testbench for 1-bit Full Adder
--- Drives all 8 input combinations for 100% coverage
--- Lab 02 Exercise 01
-
 entity full_adder_tb is
-end full_adder_tb;
+end entity;
 
-architecture bench of full_adder_tb is
+architecture bhv of full_adder_tb is
 
-    component full_adder
-        port(a, b, cin : in  bit;
-             s, cout   : out bit);
-    end component;
+signal a_tb, b_tb, ci_tb, s_tb, co_tb : bit;
 
-    signal a_tb, b_tb, cin_tb : bit;
-    signal s_tb, cout_tb      : bit;
+component full_adder is
+    port(
+        a  : in bit;
+        b  : in bit;
+        ci : in bit;
+        s  : out bit;
+        co : out bit
+    );
+end component;
 
 begin
 
-    uut: full_adder port map(a    => a_tb,
-                             b    => b_tb,
-                             cin  => cin_tb,
-                             s    => s_tb,
-                             cout => cout_tb);
+UUT: full_adder
+    port map(
+        a  => a_tb,
+        b  => b_tb,
+        ci => ci_tb,
+        s  => s_tb,
+        co => co_tb
+    );
 
-    a_tb   <= '0', '0' after 10 ns, '0' after 20 ns, '0' after 30 ns,
-              '1' after 40 ns, '1' after 50 ns, '1' after 60 ns, '1' after 70 ns;
+stimulus: process
+begin
 
-    b_tb   <= '0', '0' after 10 ns, '1' after 20 ns, '1' after 30 ns,
-              '0' after 40 ns, '0' after 50 ns, '1' after 60 ns, '1' after 70 ns;
+    a_tb <= '0';
+    b_tb <= '0';
+    ci_tb <= '0';
+    wait for 10 ns;
 
-    cin_tb <= '0', '1' after 10 ns, '0' after 20 ns, '1' after 30 ns,
-              '0' after 40 ns, '1' after 50 ns, '0' after 60 ns, '1' after 70 ns;
+    a_tb <= '0';
+    b_tb <= '0';
+    ci_tb <= '1';
+    wait for 10 ns;
 
-end bench;
+    a_tb <= '0';
+    b_tb <= '1';
+    ci_tb <= '0';
+    wait for 10 ns;
+
+    a_tb <= '0';
+    b_tb <= '1';
+    ci_tb <= '1';
+    wait for 10 ns;
+
+    a_tb <= '1';
+    b_tb <= '0';
+    ci_tb <= '0';
+    wait for 10 ns;
+
+    a_tb <= '1';
+    b_tb <= '0';
+    ci_tb <= '1';
+    wait for 10 ns;
+
+    a_tb <= '1';
+    b_tb <= '1';
+    ci_tb <= '0';
+    wait for 10 ns;
+
+    a_tb <= '1';
+    b_tb <= '1';
+    ci_tb <= '1';
+    wait for 10 ns;
+
+    wait;
+
+end process;
+
+end bhv;
