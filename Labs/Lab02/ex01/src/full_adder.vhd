5bc0fa5d2ce0d3da013db5ce8fbc39556d4c9dab
Razin936 <md-jubair-salehin.razin@stud.hshl.de>
2026-05-23 13:47:16 +0200

    Update full_adder.vhd Razin

--- Labs/Lab02/ex01/src/full_adder.vhd
+++ Labs/Lab02/ex01/src/full_adder.vhd
@@ -1,26 +1,41 @@
--- 1-bit Full Adder (structural)
--- Built from two half adders and an OR gate
--- Lab 02 Exercise 01
-
 entity full_adder is
-    port(a, b, cin : in  bit;
-         s, cout   : out bit);
+    port(
+        a, b, cin : in bit;
+        s, cout   : out bit
+    );
 end full_adder;
 
 architecture structural of full_adder is
 
-    component half_adder
-        port(a, b  : in  bit;
-             s, co : out bit);
+    component half_adder is
+        port(
+            a, b : in bit;
+            s, co : out bit
+        );
     end component;
 
-    signal s1, c1, c2 : bit;
+    signal s1  : bit;
+    signal co1 : bit;
+    signal co2 : bit;
 
 begin
 
-    HA1: half_adder port map(a => a,  b => b,   s => s1, co => c1);
-    HA2: half_adder port map(a => s1, b => cin, s => s,  co => c2);
+    HA1: half_adder
+        port map(
+            a  => a,
+            b  => b,
+            s  => s1,
+            co => co1
+        );
 
-    cout <= c1 or c2;
+    HA2: half_adder
+        port map(
+            a  => s1,
+            b  => cin,
+            s  => s,
+            co => co2
+        );
+
+    cout <= co1 or co2;
 
 end structural;
