19b41d4e32e9d4f628516a3c76247a351d81df27
Razin936 <md-jubair-salehin.razin@stud.hshl.de>
2026-05-23 13:52:25 +0200

    Update full_adder_bhv_tb.vhd Razin

--- Labs/Lab01/ex02/tb/full_adder_bhv_tb.vhd
+++ Labs/Lab01/ex02/tb/full_adder_bhv_tb.vhd
@@ -1,35 +1,79 @@
-entity full_adder_bhv_tb is
-end full_adder_bhv_tb;
+entity full_adder_tb is
+end entity;
 
-architecture bench of full_adder_bhv_tb is
+architecture bhv of full_adder is
 
-    -- Declare the design under test as a component
-    component full_adder_bhv
-        port(a, b, ci : in bit;
-             s, co   : out bit);
-    end component;
+signal 
+a_tb, b_tb, ci_tb, s_tb, co_tb: bit;
 
-    -- Internal signals to wire to the DUT
-    signal a_tb, b_tb, ci_tb : bit;
-    signal s_tb, co_tb       : bit;
+component full_adder is
+
+port 
+( a: in bit;
+b: in bit;
+ci: in bit;
+s: out bit;
+co: out bit
+);
+
+end component;
 
 begin
 
-    -- Place one full_adder_bhv and connect its pins
-    uut: full_adder_bhv port map(a  => a_tb,
-                                 b  => b_tb,
-                                 ci => ci_tb,
-                                 s  => s_tb,
-                                 co => co_tb);
+UUT: full_adder
+port map (
+a => a_tb,
+b => b_tb,
+ci => ci_tb,
+s => s_tb,
+co => co_tb
+);
 
-    -- Drive all 8 input combinations: 000, 001, 010, 011, 100, 101, 110, 111
-    a_tb  <= '0', '0' after 10 ns, '0' after 20 ns, '0' after 30 ns,
-             '1' after 40 ns, '1' after 50 ns, '1' after 60 ns, '1' after 70 ns;
+stimulus: process
+begin 
 
-    b_tb  <= '0', '0' after 10 ns, '1' after 20 ns, '1' after 30 ns,
-             '0' after 40 ns, '0' after 50 ns, '1' after 60 ns, '1' after 70 ns;
+a_tb <= '0';
+b_tb <= '0';
+ci_tb <= '0';
+wait for 10 ns;
 
-    ci_tb <= '0', '1' after 10 ns, '0' after 20 ns, '1' after 30 ns,
-             '0' after 40 ns, '1' after 50 ns, '0' after 60 ns, '1' after 70 ns;
+a_tb <= '0';
+b_tb <= '0';
+ci_tb <= '1';
+wait for 10 ns;
 
-end bench;
+a_tb <= '0';
+b_tb <= '1';
+ci_tb <= '0';
+wait for 10 ns;
+
+a_tb <= '0';
+b_tb <= '1';
+ci_tb <= '1';
+wait for 10 ns;
+
+a_tb <= '1';
+b_tb <= '0';
+ci_tb <= '0';
+wait for 10 ns;
+
+a_tb <= '1';
+b_tb <= '0';
+ci_tb <= '1';
+wait for 10 ns;
+
+a_tb <= '1';
+b_tb <= '1';
+ci_tb <= '0';
+wait for 10 ns;
+
+a_tb <= '1';
+b_tb <= '1';
+ci_tb <= '1';
+wait for 10 ns;
+
+wait;
+
+end process;
+
+end bhv;
