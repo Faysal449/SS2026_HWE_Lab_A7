a5fd48ada6b765fe27609f94bebd85d4e96274f7
Razin936 <md-jubair-salehin.razin@stud.hshl.de>
2026-07-02 16:02:52 +0200

    updated

--- Projects/VHDL & PCB/QuestaSim/rsu_controller.vhd
+++ Projects/VHDL & PCB/QuestaSim/rsu_controller.vhd
@@ -4,43 +4,50 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity rsu_controller is
     port (
-        clk       : in  std_logic;
-        reset     : in  std_logic;
-        tick_1s   : in  std_logic;
+        clk       : in  std_logic;  -- FPGA clock
+        reset     : in  std_logic;  -- Reset controller to IDLE
+        tick_1s   : in  std_logic;  -- 1-second enable pulse
 
-        N_req     : in  std_logic;
-        S_req     : in  std_logic;
-        E_req     : in  std_logic;
-        W_req     : in  std_logic;
+        N_req     : in  std_logic;  -- North request
+        S_req     : in  std_logic;  -- South request
+        E_req     : in  std_logic;  -- East request
+        W_req     : in  std_logic;  -- West request
 
-        N_grant   : out std_logic;
-        S_grant   : out std_logic;
-        E_grant   : out std_logic;
-        W_grant   : out std_logic;
+        N_grant   : out std_logic; -- North grant
+        S_grant   : out std_logic; -- South grant
+        E_grant   : out std_logic; -- East grant
+        W_grant   : out std_logic; -- West grant
 
-        road_code : out std_logic_vector(1 downto 0);
-        countdown : out integer range 0 to 20
+        road_code : out std_logic_vector(1 downto 0); -- 00=IDLE, 01=MAIN, 10=SIDE
+        countdown : out integer range 0 to 20          -- Current timer value
     );
 end rsu_controller;
 
 architecture Behavioral of rsu_controller is
 
+    -- FSM states
     type state_type is (IDLE, MAIN_SERVICE, MAIN_CLEAR, SIDE_SERVICE, SIDE_CLEAR);
     signal state : state_type := IDLE;
+
+    -- Timer for service and clearance periods
     signal timer : integer range 0 to 20 := 0;
 
+    -- Grouped road requests
     signal main_req : std_logic;
     signal side_req : std_logic;
 
+    -- Timing constants in seconds
     constant T_MAIN  : integer := 20;
     constant T_SIDE  : integer := 10;
     constant T_CLEAR : integer := 5;
 
 begin
 
+    -- Main road = North/South, Side road = East/West
     main_req <= N_req or S_req;
     side_req <= E_req or W_req;
 
+    -- Sequential FSM process: updates state and timer once per second
     process(clk, reset)
     begin
         if reset = '1' then
@@ -52,26 +59,19 @@ begin
 
                 case state is
 
+                    -- Wait for any road request
                     when IDLE =>
                         timer <= 0;
-
                         if main_req = '1' then
                             state <= MAIN_SERVICE;
                             timer <= T_MAIN;
-
                         elsif side_req = '1' then
                             state <= SIDE_SERVICE;
                             timer <= T_SIDE;
-
-                        else
-                            state <= IDLE;
-                            timer <= 0;
                         end if;
 
-
+                    -- Main road active; switch only if side road is waiting
                     when MAIN_SERVICE =>
-                        -- Main road continues freely if no side request exists.
-                        -- If side road requests, main road countdown starts.
                         if side_req = '1' then
                             if timer = 0 then
                                 state <= MAIN_CLEAR;
@@ -80,34 +80,27 @@ begin
                                 timer <= timer - 1;
                             end if;
                         else
-                            state <= MAIN_SERVICE;
                             timer <= T_MAIN;
                         end if;
 
-
+                    -- Clearance time after main road
                     when MAIN_CLEAR =>
-                        -- No grants during clear time.
                         if timer = 0 then
                             if side_req = '1' then
                                 state <= SIDE_SERVICE;
                                 timer <= T_SIDE;
-
                             elsif main_req = '1' then
                                 state <= MAIN_SERVICE;
                                 timer <= T_MAIN;
-
                             else
                                 state <= IDLE;
-                                timer <= 0;
                             end if;
                         else
                             timer <= timer - 1;
                         end if;
 
-
+                    -- Side road active; switch only if main road is waiting
                     when SIDE_SERVICE =>
-                        -- Side road continues freely if no main request exists.
-                        -- If main road requests, side road countdown starts.
                         if main_req = '1' then
                             if timer = 0 then
                                 state <= SIDE_CLEAR;
@@ -116,25 +109,20 @@ begin
                                 timer <= timer - 1;
                             end if;
                         else
-                            state <= SIDE_SERVICE;
                             timer <= T_SIDE;
                         end if;
 
-
+                    -- Clearance time after side road
                     when SIDE_CLEAR =>
-                        -- No grants during clear time.
                         if timer = 0 then
                             if main_req = '1' then
                                 state <= MAIN_SERVICE;
                                 timer <= T_MAIN;
-
                             elsif side_req = '1' then
                                 state <= SIDE_SERVICE;
                                 timer <= T_SIDE;
-
                             else
                                 state <= IDLE;
-                                timer <= 0;
                             end if;
                         else
                             timer <= timer - 1;
@@ -145,36 +133,46 @@ begin
         end if;
     end process;
 
-
+    -- Combinational output process
     process(state, N_req, S_req, E_req, W_req, timer)
     begin
+        -- Default outputs
         N_grant <= '0';
         S_grant <= '0';
         E_grant <= '0';
         W_grant <= '0';
-
         road_code <= "00";
         countdown <= timer;
 
         case state is
 
+            -- Grant only requested directions on the main road
             when MAIN_SERVICE =>
                 road_code <= "01";
-                N_grant <= N_req;
-                S_grant <= S_req;
+                if N_req = '1' then
+                    N_grant <= '1';
+                end if;
+                if S_req = '1' then
+                    S_grant <= '1';
+                end if;
 
+            -- No grants during clear time
             when MAIN_CLEAR =>
                 road_code <= "01";
-                -- clear time: no grant
 
+            -- Grant only requested directions on the side road
             when SIDE_SERVICE =>
                 road_code <= "10";
-                E_grant <= E_req;
-                W_grant <= W_req;
+                if E_req = '1' then
+                    E_grant <= '1';
+                end if;
+                if W_req = '1' then
+                    W_grant <= '1';
+                end if;
 
+            -- No grants during clear time
             when SIDE_CLEAR =>
                 road_code <= "10";
-                -- clear time: no grant
 
             when others =>
                 road_code <= "00";
